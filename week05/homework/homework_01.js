0efae1b7c6d123f149281cadbd19261dfdb01f13
niur735 <31444265+niur735@users.noreply.github.com>
2019-10-11 13:47:59 -0400

    Update homework_01.js

--- week05/homework/homework_01.js
+++ week05/homework/homework_01.js
@@ -11,3 +11,17 @@ function draw() {
         drawBlueCircle(width / 2, height / 2, 100);
     }
 }
+
+function drawRedCircle(x,y,r){
+  noStroke();
+  fill(255,0,0);
+  ellipse(x, y, r);
+  
+}
+
+function drawBlueCircle(x,y,r){
+  fill(0,0,255);
+  noStroke();
+  ellipse(x, y, r);
+  
+}
